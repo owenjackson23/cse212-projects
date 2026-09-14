a7e339f47c676acc6c6f4de299bcaade8b0d29d9
owenjackson23 <owenryanjackson@icloud.com>
2026-09-13 19:58:59 -0700

    Enqueue method now correctly adds people to the back instead of the front

--- week02/code/TakingTurnsQueue_Tests.cs
+++ week02/code/TakingTurnsQueue_Tests.cs
@@ -11,8 +11,7 @@ public class TakingTurnsQueueTests
     // Scenario: Create a queue with the following people and turns: Bob (2), Tim (5), Sue (3) and
     // run until the queue is empty
     // Expected Result: Bob, Tim, Sue, Bob, Tim, Sue, Tim, Sue, Tim, Tim
-    // Defect(s) Found: Each person took one less turn than they should have.
-    //      In the GetNextPerson method, person.Turns > 1 should be person.Turns > 0
+    // Defect(s) Found: The enqueue function was adding each person to the front of the queue instead of the back.
     public void TestTakingTurnsQueue_FiniteRepetition()
     {
         var bob = new Person("Bob", 2);
