8ef9b5984c901fb411b2b6904a5ec4c66ebdacb7
owenjackson23 <owenryanjackson@icloud.com>
2026-09-19 10:19:22 -0700

    TakingTurnsQueue is not properly enqueuing people with infinite turns

--- week02/code/TakingTurnsQueue_Tests.cs
+++ week02/code/TakingTurnsQueue_Tests.cs
@@ -85,7 +85,7 @@ public class TakingTurnsQueueTests
     // Scenario: Create a queue with the following people and turns: Bob (2), Tim (Forever), Sue (3)
     // Run 10 times.
     // Expected Result: Bob, Tim, Sue, Bob, Tim, Sue, Tim, Sue, Tim, Tim
-    // Defect(s) Found: 
+    // Defect(s) Found: When turns are set to 0 (forever), the person is not enqueued.
     public void TestTakingTurnsQueue_ForeverZero()
     {
         var timTurns = 0;
