3e9db773ba2111a8f27c6424ba41f2dd9a9ab719
owenjackson23 <owenryanjackson@icloud.com>
2026-09-19 11:50:06 -0700

    implemented TestMethod TestPriorityQueue_SamePriority to test PriorityQueue when all items have the same priority

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -6,13 +6,49 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 public class PriorityQueueTests
 {
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
+    // Scenario: Create a queue in which all items have the same priority: Bod (1), Tim (1), Sue (1), George (1) and
+    // run until the queue is empty
+    // Expected Result: bob, tim, sue, george
     // Defect(s) Found: 
-    public void TestPriorityQueue_1()
+    public void TestPriorityQueue_SamePriority()
     {
+        var bob = new PriorityItem("Bob", 1);
+        var tim = new PriorityItem("Tim", 1);
+        var sue = new PriorityItem("Sue", 1);
+        var george = new PriorityItem("George", 1);
+
+        PriorityItem[] expectedResult = [bob, tim, sue, george];
+
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        priorityQueue.Enqueue(bob.Value, bob.Priority);
+
+        for (int i = expectedResult.Length; i > 0; i--)
+        {
+
+            var item = priorityQueue.Dequeue();
+            Assert.AreEqual(expectedResult[i].Value, item);
+        }
+
+        try
+        {
+            priorityQueue.Dequeue();
+            Assert.Fail("Exception should have been thrown.");
+        }
+        catch (InvalidOperationException e)
+        {
+            Assert.AreEqual("The queue is empty.", e.Message);
+        }
+        catch (AssertFailedException)
+        {
+            throw;
+        }
+        catch (Exception e)
+        {
+            Assert.Fail(
+                 string.Format("Unexpected exception of type {0} caught: {1}",
+                                e.GetType(), e.Message)
+            );
+        }
     }
 
     [TestMethod]
